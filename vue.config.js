7e1d403ac6b36f930bd6400e8a4087f044dd8c19
chengyuefeng <3162682561@qq.com>
2024-04-16 16:38:40 +0800

    feat: 新增项目启动devServer

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0'
+  },
   css: {
     loaderOptions: {
       sass: {
