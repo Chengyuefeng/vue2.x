68ddd28882026d47cfc9cc13c3f1c9972521f097
chengyuefeng <3162682561@qq.com>
2024-04-22 17:07:51 +0800

    feat: 学习canvas

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -83,81 +83,28 @@ export default {
       canvas = document.getElementById("pata-tap")
       if (canvas.getContext) {
         ctx = canvas.getContext("2d")
-        let lineCap = ["butt", "round", "square"]
 
-
-        for (var i = 0; i < 6; i++) {
-          for (var j = 0; j < 6; j++) {
-            ctx.fillStyle =
-              "rgba(" +
-              Math.floor(255 - 42.5 * i) +
-              "," +
-              Math.floor(255 - 42.5 * j) +
-              ", 50)";
-            ctx.fillRect(j * 25 + 20, i * 25 + 20, 25, 25);
-          }
-        }
-
-        ctx.fillStyle = "#FFF";
-        // 设置透明度值
-        ctx.globalAlpha = 0.1;
-
-        // 画半透明圆
-        for (var k = 0; k < 9; k++) {
-          ctx.beginPath();
-          ctx.arc(95, 275, 10 * k, 0, Math.PI * 2, true);
-          ctx.fill();
-        }
-
-        ctx.globalAlpha = 1;
-        for (var l = 0; l < 11; l++) {
-          ctx.lineWidth = 1 + l;
-          ctx.beginPath();
-          ctx.moveTo(20 + l * 16, 400);
-          ctx.lineTo(20 + l * 16, 540);
-          ctx.stroke();
-        }
-        
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "#09f";
-        ctx.beginPath();
-        ctx.moveTo(210, 20);
-        ctx.lineTo(340, 20);
-        ctx.moveTo(210, 170);
-        ctx.lineTo(340, 170);
-        ctx.stroke();
-        ctx.strokeStyle = "black";
-        for (var a = 0; a < lineCap.length; a++) {
-          ctx.lineWidth = 15;
-          ctx.lineCap = lineCap[a];
-          ctx.beginPath();
-          ctx.moveTo(225 + a * 50, 20);
-          ctx.lineTo(225 + a * 50, 170);
-          ctx.stroke();
-        }
-
-        ctx.lineWidth = 1
-
-        var lineargradient = ctx.createLinearGradient(450, 20, 450, 120);
-        lineargradient.addColorStop(0, "white");
-        lineargradient.addColorStop(.5, "rgba(0, 0, 0, .1)");
-        lineargradient.addColorStop(1, "black");
-        ctx.fillStyle = lineargradient
-        ctx.fillRect(400, 20, 100, 100);
-
-        var img = new Image();
-        img.src = require('@/assets/images/xiaohua.jpeg')
-        img.onload = function () {
-          // 创建图案
-          var ptrn = ctx.createPattern(img, "no-repeat");
-          ctx.fillStyle = ptrn;
-          ctx.fillRect(0, 0, 750, 850);
-        };
-        // this.march()
         // drawing code here
       } else {
         // canvas-unsupported code here
       }
+    },
+
+    drawStar(ctx, r) {
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(r, 0);
+      for (var i = 0; i < 9; i++) {
+        ctx.rotate(Math.PI / 5);
+        if (i % 2 == 0) {
+          ctx.lineTo((r / 0.525731) * 0.200811, 0);
+        } else {
+          ctx.lineTo(r, 0);
+        }
+      }
+      ctx.closePath();
+      ctx.fill();
+      ctx.restore();
     },
 
     draw() {
