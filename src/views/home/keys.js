7434ec6797ac4c0453e3e07639361357dd95280c
chengyuefeng <3162682561@qq.com>
2024-04-24 10:08:01 +0800

    feat: canvas动画新增效果

--- src/views/home/keys.js
+++ src/views/home/keys.js
@@ -1,21 +1,6 @@
 export default (ctx) => ({
   q: {
-    canvasDraw(canvas) {
-      const startX = ctx.pataTapWidth * 0.2 / 2
-      const startY = (ctx.pataTapHeight - 200) / 2
-      let width = 0
-      canvas.fillStyle = '#FFF'
-      const time = setInterval(() => {
-        if (width < ctx.pataTapWidth * 0.8) {
-          canvas.clearRect(0, 0, ctx.pataTapWidth, ctx.pataTapHeight)
-          width += 20
-          canvas.fillRect(startX, startY, width, 200)
-        } else {
-          clearInterval(time)
-          canvas.clearRect(0, 0, ctx.pataTapWidth, ctx.pataTapHeight)
-        }
-      }, 1)
-    }
+    
   },
   w: {
 
@@ -24,7 +9,28 @@ export default (ctx) => ({
 
   },
   r: {
-
+    canvasDraw(canvas) {
+      let type = Math.ceil(Math.random() * 10) % 2 ? 'toLeft' : 'toRight'
+      let width = 0
+      const startX = type === 'toRight' ? ctx.pataTapWidth * .1 : ctx.pataTapWidth * .9
+      const startY = (ctx.pataTapHeight - 300) / 2
+      
+      canvas.fillStyle = '#FFF'
+      const time = setInterval(() => {
+        if (Math.abs(width) < ctx.pataTapWidth * .8) {
+          canvas.clearRect(0, 0, ctx.pataTapWidth, ctx.pataTapHeight)
+          if (type === 'toRight') {
+            width += 25
+          } else {
+            width -= 25
+          }
+          canvas.fillRect(startX, startY, width, 300)
+        } else {
+          clearInterval(time)
+          canvas.clearRect(0, 0, ctx.pataTapWidth, ctx.pataTapHeight)
+        }
+      }, 1)
+    }
   },
   t:{
 
